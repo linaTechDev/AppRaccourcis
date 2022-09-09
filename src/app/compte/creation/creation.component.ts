a430d8834d88a498e98a97a927d13a082645eff0
20331622@claurendeau.qc.ca <2033162@claurendeau.qc.ca>
2022-09-08 20:27:20 -0400

    front-end (creation compte)

---
+++ src/app/compte/creation/creation.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  templateUrl: 'creation.component.html',
+  styleUrls: ['creation.component.css']
+})
+export class CreationComponent implements OnInit {
+  form!: FormGroup;
+  loading = false;
+  submitted = false;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    this.form = this.formBuilder.group({
+      prenom: ['', Validators.required],
+      nomFamille: ['', Validators.required],
+      nomUtilisateur: ['', Validators.required],
+      motPasse: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  // convenience getter for easy access to form fields
+  get f() { return this.form.controls; }
+
+  onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.form.invalid) {
+      return;
+    }
+
+    this.loading = true;
+  }
+}
