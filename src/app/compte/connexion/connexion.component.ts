9418af2520edbe5f9d4352061f8256275223920e
20331622@claurendeau.qc.ca <2033162@claurendeau.qc.ca>
2022-09-08 20:25:53 -0400

    front-end (connexion compte)

---
+++ src/app/compte/connexion/connexion.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  templateUrl: 'connexion.component.html',
+  styleUrls: ['connexion.component.css']
+})
+export class ConnexionComponent implements OnInit {
+  form!: FormGroup;
+  loading = false;
+  submitted = false;
+  returnUrl!: string;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    this.form = this.formBuilder.group({
+      nomUtilisateur: ['', Validators.required],
+      MotDePasse: ['', Validators.required]
+    });
+
+    // get return url from route parameters or default to '/'
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+  }
+
+  // convenience getter for easy access to form fields
+  get f() { return this.form.controls; }
+
+  onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.form.invalid) {
+      return;
+    }
+
+    this.loading = true;
+  }
+}
