ab30afcdf4e72aa61ceb62651dab3eb0517066ae
20331622@claurendeau.qc.ca <2033162@claurendeau.qc.ca>
2022-09-09 02:05:21 -0400

    backend - service (utilisateur)

---
+++ spring/src/main/java/com/lina/spring/service/ServiceUtilisateur.java
@@ -0,0 +1,65 @@
+package com.lina.spring.service;
+
+import com.lina.spring.dtos.UtilisateurDto;
+import com.lina.spring.models.Utilisateur;
+import com.lina.spring.repository.UtilisateurRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ServiceUtilisateur {
+
+  private UtilisateurRepository utilisateurRepository;
+
+  public ServiceUtilisateur(UtilisateurRepository utilisateurRepository) {
+    this.utilisateurRepository = utilisateurRepository;
+  }
+
+  public UtilisateurDto saveUtilisateur(
+    String prenom,
+    String nomFamille,
+    String nomUtilisateur,
+    String motPasse
+  ) {
+    Utilisateur utilisateur = utilisateurRepository.save(new Utilisateur(
+      prenom,
+      nomFamille,
+      nomUtilisateur,
+      motPasse
+    ));
+    return utilisateur.toUtilisateurDto();
+  }
+
+  public UtilisateurDto saveUtilisateur(Utilisateur utilisateur) {
+    return utilisateurRepository.save(utilisateur).toUtilisateurDto();
+  }
+
+  public boolean existsByNomUtilisateur(String nomUtilisateur) {
+    return utilisateurRepository.existsByNomUtilisateur(nomUtilisateur);
+  }
+
+  public UtilisateurDto findByNomUtilisateur(String nomUtilisateur) {
+    Utilisateur utilisateur = utilisateurRepository.findByNomUtilisateur(nomUtilisateur);
+    if (utilisateur != null) {
+      return utilisateur.toUtilisateurDto();
+    }
+    return null;
+  }
+
+  public boolean validateAuthentification(String nomUtilisateur, String motPasse) {
+    Utilisateur utilisateur = findByNomUtilisateur(nomUtilisateur).toUtilisateur();
+    if (utilisateur != null) {
+      return motPasse == utilisateur.getMotPasse();
+    }
+    return false;
+  }
+
+  /*@Override
+  public UserDetails loadUserByUsername(String nomUtilisateur) throws UsernameNotFoundException {
+    Utilisateur utilisateur = utilisateurRepository.findByNomUtilisateur(nomUtilisateur)
+      .orElseThrow(() -> new UsernameNotFoundException("Utilisateur non trouvé avec le nom d'utilisateur: " + nomUtilisateur));
+    return ;
+  }*/
+}
