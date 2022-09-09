a786e7fedbcabea2b029dadd12282e30a1a8a592
20331622@claurendeau.qc.ca <2033162@claurendeau.qc.ca>
2022-09-08 20:31:26 -0400

    nettoyage du code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from "@angular/common/http";
