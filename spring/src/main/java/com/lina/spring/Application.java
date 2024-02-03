8f6af37cf2548cb65b5dbc18330bc832b24cf9b8
linaTechDev <lina.ly.2018@outlook.fr>
2024-02-03 16:50:46 -0500

    Essaie de déployer application angular et spring sur tomcat mais c'est encore local

--- spring/src/main/java/com/lina/spring/Application.java
+++ spring/src/main/java/com/lina/spring/Application.java
@@ -3,16 +3,25 @@ package com.lina.spring;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorViewResolver;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Collections;
 
 @SpringBootApplication
-public class Application implements CommandLineRunner {
+public class Application extends SpringBootServletInitializer {
+
+  @Bean
+  public ErrorViewResolver customErrorViewResolver() {
+    final ModelAndView redirectToIndexHtml = new ModelAndView("forward:/index.html",
+      Collections.emptyMap(), HttpStatus.OK);
+    return (request, status, model) -> status == HttpStatus.NOT_FOUND ? redirectToIndexHtml : null;
+  }
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
-  @Override
-  public void run(String... args) throws Exception {
-    System.out.println("Running...");
-  }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'http://localhost:9090/appRaccourcisApi'
+  /*apiUrl: 'http://localhost:8080'*/
 };
